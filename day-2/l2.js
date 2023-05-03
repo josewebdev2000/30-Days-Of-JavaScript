9df8841ea9303d92d016e83e57bc6c981225fe7c
josewebdev2000 <josewebdev@proton.me>
2023-05-03 12:57:55 -0400

    Day 2 JS Challenged Completed

--- day-2/l2.js
+++ day-2/l2.js
@@ -4,6 +4,45 @@ function main2()
     console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
     console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"");
 
+    if (!(typeof('10') == typeof(10)))
+    {
+        console.log(typeof(parseInt('10')) == typeof(10));
+    }
+
+    if (!(parseFloat('9.8') == 10))
+    {
+        console.log(Math.ceil(parseFloat('9.8')) == 10);
+    }
+
+    console.log("python".includes("on") && "jargon".includes("on"));
+    console.log("I hope this course is not full of jargon".includes("jargon"));
+
+    console.log(getRandomNumberInRange(0, 100));
+    console.log(getRandomNumberInRange(50, 100));
+    console.log(getRandomNumberInRange(0, 255));
+    
+    let js = "JavaScript";
+
+    console.log(js[getRandomNumberInRange(0, js.length - 1)]);
+    
+    console.log("1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125");
+
+    let phrase = "You cannot end a sentence with because because because is a conjunction";
+    let becauseRegex = /\bbecause\b/g;
+    let matches = matchAllOccurences(becauseRegex, phrase);
+
+    console.log(matches.join(" "));
+
+}
+
+function getRandomNumberInRange(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function matchAllOccurences(regex, phrase)
+{
+    return phrase.match(regex);
 }
 
 document.addEventListener("DOMContentLoaded", () => main2());
--- day-2/l3.js
+++ day-2/l3.js
@@ -1,5 +1,56 @@
 function main3()
 {
+    console.log("\n\nLevel 3 Challenges\n\n");
+
+    let loveSentence = "Love is the best thing in this world. Some found their love and some are still looking for their love";
+
+    let loveRegex = /\blove\b/g;
+    let matches = loveSentence.match(loveRegex);
+    console.log(matches.length);
+
+    let becauseRegex = /\bbecause\b/g;
+    let countOccurrences = matchAllOccurences(becauseRegex, 'You cannot end a sentence with because because because is a conjunction').length;
+    console.log(countOccurrences);
+
+    const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+
+    const specialRegex = /[^\w\s]/g;
+
+    let newSentence = sentence.replace(specialRegex, '');
+    
+    const words = newSentence.split(" ");
+    let repsNWords = {};
+
+    for (let i = 0; i < words.length; i++)
+    {
+        const word = words[i];
+        repsNWords[word] = repsNWords[word] ? repsNWords[word] + 1 : 1;
+    }
+
+    let maxCount = 0;
+    let mostFrequent;
+
+    for (const word in repsNWords) {
+        if (repsNWords[word] > maxCount) {
+          maxCount = repsNWords[word];
+          mostFrequent = word;
+        }
+      }
+
+    console.log(mostFrequent);
+
+    const salaryInfo = "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+    const numRegex = /\d+/g;
+
+    const nums = salaryInfo.match(numRegex);
+
+    for (let i = 0; i < nums.length; i++)
+    {
+        nums[i] = parseInt(nums[i]);
+    }
+
+    const totalAnnualSalary = nums[0] * 12 + nums[1] + nums[2] * 12;
+    console.log(totalAnnualSalary);
 
 }
 
